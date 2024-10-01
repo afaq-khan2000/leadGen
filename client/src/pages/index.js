f3489eea034b461af948fc266bb9f265e845738e
afaq-khan2000 <kafaq885@gmail.com>
2024-10-02 01:02:18 +0500

    added login signup pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,1 +1,2 @@
-// export { default as Home } from "./Home";
+export { default as SignupPage } from "./SignupPage";
+export { default as LoginPage } from "./LoginPage";
--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "../pages";
+import { LoginPage, SignupPage } from "../pages";
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
